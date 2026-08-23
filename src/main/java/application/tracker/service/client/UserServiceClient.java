6058f0b618711e5cb0cb629b11b14b40682652ff
Nishu <nishumca22@gmail.com>
2026-08-23 20:41:34 +0530

    feat(tracker): enrich status events with user details

---
+++ src/main/java/application/tracker/service/client/UserServiceClient.java
@@ -0,0 +1,30 @@
+package application.tracker.service.client;
+
+import application.tracker.service.dto.UserDetailsDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Slf4j
+public class UserServiceClient {
+
+    private final RestClient restClient;
+
+    public UserServiceClient(@Qualifier("userRestClient") RestClient restClient){
+        this.restClient = restClient;
+    }
+
+    public UserDetailsDto getUserDetails(Long userId, String token){
+        String forwardedtoken = token.startsWith("Bearer ") ? token : "Bearer " + token;
+        UserDetailsDto response = restClient.get()
+                .uri("http://USER-SERVICE/api/users/{userId}", userId)
+                .header("Authorization", forwardedtoken)
+                .retrieve()
+                .body(UserDetailsDto.class);
+        return  response;
+    }
+
+}
--- src/main/java/application/tracker/service/service/JobApplicationService.java
+++ src/main/java/application/tracker/service/service/JobApplicationService.java
@@ -11,7 +11,7 @@ public interface JobApplicationService {
     JobApplicationResponse createApplication(JobApplicationRequest request, Long userId, String token);
     Page<JobApplicationResponse> getAllApplications(Long userId, int page, int size);
     JobApplicationResponse getApplicationById(Long id, Long userId);
-    JobApplicationResponse updateStatus(Long id, Long userId, UpdateStatusRequest request) ;
+    JobApplicationResponse updateStatus(Long id, Long userId, UpdateStatusRequest request, String token) ;
     void deleteApplication(Long id, Long userId);
     ReuseRecommendation checkReuseRecommendation(String newJdText, Long userId);
     void generateAndSaveEmbedding(Long applicationId, String jobDesciption);
